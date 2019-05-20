10e5bb3bcfb2fae107d76aa6317c22016f6d9662
vhqd <www.zhangqiangwo@qq.com>
2019-05-20 21:06:08 +0800

    add dbConfig

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var { Mongoose } = require('./untils/config')
 
 var app = express();
 
@@ -14,18 +15,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/public',express.static(path.join(__dirname, '/public')));
-app.use('/admin',express.static(path.join(__dirname, '/views/index.html')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/admin', express.static(path.join(__dirname, '/views/index.html')));
 app.use('/index', express.static(path.join(__dirname, '/views/client.html')));
 app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
+Mongoose.connect();
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
